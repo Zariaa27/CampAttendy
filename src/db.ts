ec01769a946676041002ff159660f1fc7da8bfb4
Alexis Trudeau <75556887+Zariaa27@users.noreply.github.com>
2023-06-20 23:23:48 -0400

    update from last commit.

--- src/db.ts
+++ src/db.ts
@@ -2,17 +2,26 @@ import mysql from 'mysql2';
 
 export default class DB {
 	private static readonly db: mysql.Connection = mysql.createConnection({
-		host: 'example',
-		user: 'example',
-		password: 'example',
-		database: 'example'
+		host: 'techno.artivain.com',
+		user: 'u26_1qWd3Vkamf',
+		password: 'nfAOQ.obYqwlk7J.^1uGrIg^',
+		database: 's26_campattendy'
 	});
 
-    public static async getOccupant(name: string): Promise<string> {
+    public static async getOccupant(name: string): Promise<Occupant> {
         return new Promise((resolve, reject) => {
 			this.db.query<Occupant[]>(`SELECT * FROM occupants WHERE name = '${name}'`, (error, result) => {
 				if (error) reject(error);
-				resolve(result[0]?.value);
+				resolve(result[0]);
+			});
+		});
+    }
+
+	public static async getAllFromGroup(group: string): Promise<Occupant[]> {
+        return new Promise((resolve, reject) => {
+			this.db.query<Occupant[]>(`SELECT * FROM occupants WHERE \`group\` = ${group}`, (error, result) => {
+				if (error) reject(error);
+				resolve(result);
 			});
 		});
     }
@@ -21,7 +30,7 @@ export default class DB {
 export interface Occupant extends mysql.RowDataPacket {
     id: number;
     name: string;
-    age: string;
+    age: number;
     group: string;
     allergies: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
 import DB from "./db";
+import chalk from "chalk";
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+});
 
-let occupant = await DB.getOccupant("John Doe")
+fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
+});
+
+fastify.get('/getOccupant:name', async function (request, reply) {
+    let name = (request.params as { [key: string]: string }).name.replace(":", "")
+    let occupant = await DB.getOccupant(name)
+    reply.send(occupant)
+});
+
+fastify.get('/getAllFromGroup:group', async function (request, reply) {
+  let group = (request.params as { [key: string]: string }).group.replace(":", "")
+  console.log(group)
+  let dbReply = await DB.getAllFromGroup(group)
+  console.log(dbReply)
+  reply.send(dbReply)
+});
+
+
+fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    console.log(chalk.green.bold('Server is now listening on localhost:3000'))
+});
